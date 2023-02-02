1514751b28ff6a1212139f758610791ccb6419e4
Sakib Mahmud <75196289+smsakib15@users.noreply.github.com>
2023-02-02 22:47:00 +0600

    Admin Panel part completed

---
+++ Admin/viewsubcategory.php
@@ -0,0 +1,99 @@
+<?php if(!isset($_SESSION)) { session_start(); } ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+<link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
+
+<link href="style.css" rel="stylesheet" type="text/css" />
+
+<link href="../css/bootstrap.css" rel='stylesheet' type='text/css'/>
+<link href="../css/style.css" rel="stylesheet" type="text/css" media="all"/>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+
+
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!--js--> 
+<script src="js/jquery.min.js"></script>
+
+<!--/js-->
+<!--animated-css-->
+<link href="../css/animate.css" rel="stylesheet" type="text/css" media="all">
+<script src="../js/wow.min.js"></script>
+<script>
+ new WOW().init();
+</script>
+<!--/animated-css-->
+</head>
+<body>
+<!--header-->
+<!--sticky-->
+<?php
+if($_SESSION['loginstatus']=="")
+{
+	header("location:loginform.php");
+}
+?>
+
+
+<?php include('function.php'); ?>
+<?php
+if(isset($_POST["sbmt"]))
+{
+	$cn=makeconnection();
+	$s="insert into category(Cat_name) values('" . $_POST["t1"] ."')";
+	mysqli_query($cn,$s);
+	
+	echo "<script>alert('Record Save');</script>";
+}
+?>
+
+<?php include('top.php'); ?>
+<!--/sticky-->
+<div>
+<div style="float:left; color:#4e4039; font-size:16px; padding-left:20px;">
+<?php include('left.php'); ?>
+</div>
+</div>
+
+<div style="padding: 110px 0px 0px 460px;">
+	<form method="post">
+	<table border="0" width="400px" height="300px" class="tableshadow">
+	<div>
+		<tr><td class="toptd">View Subcategory</td></tr>
+		<tr><td align="center" valign="top" style="padding-top:10px;">
+		<table border="0" align="center" width="70%" >
+		<tr><td style="font-size:15px; padding:5px; font-weight:bold;">Subcategory Id</td>
+		<td style="font-size:15px; padding:5px; font-weight:bold;">Subcategory Name</td></tr>
+	
+		<?php
+	
+		$s="select * from subcategory";
+		$result=mysqli_query($cn,$s);
+		$r=mysqli_num_rows($result);
+		//echo $r;
+	
+		while($data=mysqli_fetch_array($result))
+		{
+	
+				echo "<tr><td style=' padding:5px;'>$data[0]</td><td style=' padding:5px;'>$data[1]</td></tr>";
+	
+		}
+	
+	
+		?>
+	
+		</table>
+		</td></tr>
+	</div>
+	</table>
+	
+	</form>
+</div>
+
+
+</body>
+</html>
